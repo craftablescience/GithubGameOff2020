dde3055f41dda99f326d65689220c21ac423fcf3
Brendan Lewis <brendanelewis@outlook.com>
2020-11-04 19:25:46 -0500

    Main menu

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,11 +1,7 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Game/Game
+	$Game/UI/UI.hide()
+	$Game/Game/Root.hide()
+	get_tree().paused = true
