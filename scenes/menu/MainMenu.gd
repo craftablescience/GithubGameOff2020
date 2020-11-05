27746d4bb7c1054b9f68802d8633784a8baee728
Brendan Lewis <brendanelewis@outlook.com>
2020-11-05 07:57:35 -0500

    Add web version

--- scenes/menu/MainMenu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-
-signal new_game
-
-
-func _on_ContextPanel_new_game() -> void:
-	self.emit_signal("new_game")
--- scripts/menu/BetaNotice.gd
+++
@@ -1,9 +0,0 @@
-extends AcceptDialog
-
-
-export(bool) var RELEASE : bool
-
-
-func _ready() -> void:
-	if RELEASE:
-		self.popup()
