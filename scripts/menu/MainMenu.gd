3bffd748d327dcfe7dd10929df7e442295312ed8
Brendan Lewis <brendanelewis@outlook.com>
2020-11-06 17:20:53 -0500

    Add fullscreen

--- scripts/menu/MainMenu.gd
+++ scripts/menu/MainMenu.gd
@@ -6,3 +6,6 @@ signal new_game
 
 func _on_ContextPanel_new_game() -> void:
 	self.emit_signal("new_game")
+
+func _on_Fullscreen_toggled(button_pressed: bool) -> void:
+	OS.window_fullscreen = button_pressed
