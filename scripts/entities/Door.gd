ef2591cedfc44c6e6851bbc842a8dbe3c582cce4
Brendan Lewis <brendanelewis@outlook.com>
2020-11-06 22:41:12 -0500

    Doors

---
+++ scripts/entities/Door.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+var OPEN: bool
+var INPUT_READY: bool
+
+
+func _ready() -> void:
+	$Door.play("closing")
+	self.OPEN = true
+	self.INPUT_READY = false
+
+func _process(_delta: float) -> void:
+	$StaticBody2D/CollisionShape2D.disabled = self.OPEN
+	
+	if self.INPUT_READY and Input.is_action_just_pressed("ui_select"):
+		if self.OPEN:
+			$Door.play("closing")
+		else:
+			$Door.play("opening")
+
+
+func _on_Door_animation_finished() -> void:
+	self.OPEN = !self.OPEN
+
+func _on_Area2D_body_entered(body: Node) -> void:
+	self.INPUT_READY = true
+
+func _on_Area2D_body_exited(body: Node) -> void:
+	self.INPUT_READY = false
